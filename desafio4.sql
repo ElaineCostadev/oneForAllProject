612447d998593245af8ce7f3020c587af1d397bd
ElaineCosta <elainerscosta@hotmail.com>
2022-07-26 11:25:03 -0300

    feat: incluindo query que verifica a qtd de usuarios ativos e inativos, considerando o ultimo acesso em 2021

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT nome as usuario,
+IF(MAX(YEAR(data_reproducao)) = 2021, "Usuario Ativo", "Usuario inativo") AS condicao_usuario
+from SpotifyClone.Usuario
+INNER JOIN SpotifyClone.Reproducoes ON SpotifyClone.Reproducoes.usuario_nome_id = SpotifyClone.Usuario.usuario_id
+WHERE YEAR(data_reproducao) <= 2021
+GROUP BY nome
+ORDER BY nome ASC;
+
+-- se tem o ano de 2021 está ativa se nao usuario inativo
+-- YEAR(data_reproducao) as condicao_usuario
+
+-- https://www.devmedia.com.br/forum/trazer-registro-com-data-mais-atual-para-cada-tipo-de-produto/444756
