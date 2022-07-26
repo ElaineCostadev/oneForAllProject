f1d1ef1f2fc2dcee98f4e32825b9f5955239fdd3
ElaineCosta <elainerscosta@hotmail.com>
2022-07-26 11:39:02 -0300

    fix: nome da query total_minutos

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT nome as usuario, COUNT(cancoes_musica_id) as qtde_musicas_ouvidas, ROUND((SUM(duracao_segundos) / 60), 2)
+SELECT DISTINCT nome as usuario, COUNT(cancoes_musica_id) as qtde_musicas_ouvidas, ROUND((SUM(duracao_segundos) / 60), 2) as total_minutos
 from SpotifyClone.Usuario
 INNER JOIN SpotifyClone.Reproducoes ON SpotifyClone.Reproducoes.usuario_nome_id = SpotifyClone.Usuario.usuario_id
 INNER JOIN SpotifyClone.Cancoes ON SpotifyClone.Cancoes.musica_id = SpotifyClone.Reproducoes.cancoes_musica_id
