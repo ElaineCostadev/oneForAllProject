c8eb16d9d2c3ffefc54c895a3e40b308c09cee3e
ElaineCosta <elainerscosta@hotmail.com>
2022-07-26 11:40:51 -0300

    fix: ajuste da query, Order By nome ASC para ajustar a ordem esperada pelo teste

--- desafio11.sql
+++ desafio11.sql
@@ -1,10 +1,10 @@
-SELECT musica as nome_musica, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(musica, 'Streets', 'Code Review'), 'Her Own', 'Trybe'), 'Inner Fire', 'Project'),
-'Silly', 'Nice'), 'Circus', 'Pull Request') AS novo_nome
+SELECT musica as nome_musica, REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(musica, 'Streets', 'Code Review'), 'Silly', 'Nice'), 'Circus', 'Pull Request'), 'Inner Fire', 'Project'), 'Her Own', 'Trybe') AS novo_nome
 FROM SpotifyClone.Cancoes
 WHERE musica LIKE '%Streets'
-OR musica LIKE '%Her Own'
-OR musica LIKE '%Inner Fire'
 OR musica LIKE '%Silly'
-OR musica LIKE '%Circus';
+OR musica LIKE '%Circus'
+OR musica LIKE '%Inner Fire'
+OR musica LIKE '%Her Own'
+ORDER BY musica ASC;
 
 -- REPLACE DENTRO DE REPLACE
