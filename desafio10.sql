9fb5601e2ef5c989ffdbc69a0a7d0e430fb94e27
ElaineCosta <elainerscosta@hotmail.com>
2022-07-25 22:25:50 -0300

    feat: selecionando COUNT nome e musica, fazendo join, fazendo where para filtrar pelos tipos de planos, agrupando pelas musicas de forma ASC

---
+++ desafio10.sql
@@ -0,0 +1,8 @@
+SELECT musica, COUNT(nome)
+FROM SpotifyClone.Reproducoes
+INNER JOIN SpotifyClone.Cancoes ON SpotifyClone.Cancoes.musica_id = SpotifyClone.Reproducoes.cancoes_musica_id
+INNER JOIN SpotifyClone.Usuario ON SpotifyClone.Usuario.usuario_id = SpotifyClone.Reproducoes.usuario_nome_id
+INNER JOIN SpotifyClone.Plano ON SpotifyClone.Plano.plano_id = SpotifyClone.Usuario.plano_id
+WHERE SpotifyClone.Plano.tipo IN('gratuito', 'pessoal')
+GROUP BY musica
+ORDER BY musica ASC;
